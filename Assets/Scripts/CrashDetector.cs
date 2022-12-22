0f3ee6846ba2c501ac2e438422b3bc3c596bcff9
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-12-22 18:46:43 +1100

    avoid crash happened multi times

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,15 +6,17 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashParticles;
     [SerializeField] AudioClip crashSFX;
+    bool crashed = false;
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Ground"))
+        if (col.CompareTag("Ground") && !crashed)
         {
             FindObjectOfType<PlayerController>().DisableControls();
             crashParticles.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke(nameof(ReloadScene), loadDelay);
+            crashed = true;
         }
     }
 
